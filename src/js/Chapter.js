bf165910065194f69836d49004e09d9f79b97057
Mikhail Brann <alunos13@yandex.com>
2024-06-29 10:45:43 +0300

    main chapter

--- src/js/Chapter.js
+++ src/js/Chapter.js
@@ -1,5 +1,31 @@
 export default class Chapter {
     constructor(name) {
         this.name = name;
+        this._attack = 100;
+        this._stoned = false;
+        this._attackDistance = 1;
+    }
+    get attack() {
+        return this._attack;
+    }
+    set attack(value) {
+        if(value <= 1 || value > 9) {
+            this._attack = 100;
+            return false;
+        }
+    
+        this._attackDistance = value;
+        this._attack = this._attack - ((this._attackDistance - 1) * 10);
+    }
+    get stoned() {
+        return this._stoned;
+    }
+    set stoned(value) {
+        if(value === true) {
+            this._stoned = true;
+            this._attack = this._attack - Math.log2(this._attackDistance) * 5;
+        } else {
+            this._attack = 100;
+        }
     }
 }
--- src/js/__tests__/Chapter.js
+++ src/js/__tests__/Chapter.js
@@ -2,7 +2,28 @@ import Chapter from "../Chapter";
 
 test('Chapter test', () => {
     const testObj = new Chapter("Stanislav");
-    const expected = {"name": "Stanislav"};
+    const expected = {"name": "Stanislav", "_attack": 100, "_stoned": false, "_attackDistance": 1};
     
     expect(testObj).toEqual(expected);
+});
+
+test('Chapter attack test', () => {
+    const testObj = new Chapter("Stanislav");
+    testObj.attack = 2;
+    expect(testObj.attack).toEqual(90);
+
+    testObj.attack = 1;
+    expect(testObj.attack).toEqual(100);
+});
+
+
+test('Chapter stonned test', () => {
+    const testObj = new Chapter("Stanislav");
+    testObj.attack = 2;
+    testObj.stoned = true;
+    expect(testObj.attack).toEqual(85);
+    expect(testObj.stoned).toEqual(true);
+
+    testObj.stoned = false;
+    expect(testObj.attack).toEqual(100);
 });
